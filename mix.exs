c58ce030845c1994c02113d71906be023d465670
Nirvana Project <nirvana@MYSTER.IO>
2013-03-16 18:18:50 -0300

    update .gitignore

---
+++ mix.exs
@@ -0,0 +1,25 @@
+defmodule Lively.Mixfile do
+  use Mix.Project
+
+  def project do
+    [ app: :lively,
+      version: "0.0.1",
+      deps: deps ]
+  end
+
+  # Configuration for the OTP application
+  def application do
+    [
+	 
+	 ]
+  end
+
+  # Returns the list of dependencies in the format:
+  # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
+  defp deps do 
+	[ 
+		{:couchie, github: "n1rvana/couchie"},
+ 		{:jsx,  github: "talentdeficit/jsx"}
+	]
+  end
+end
